3e9c715158940b126d271e9076cf5369453aa5d5
Fulles <ademon1998@yahoo.com>
2018-07-11 17:38:19 +0300

    Update Emulator.cs

--- Emulator.cs
+++ Emulator.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
 using System.IO.Ports;    //Nuget System.IO.Ports
 
-class PortDataReceived
+class Emulator
 {
     public static void Main()
     {
@@ -23,15 +23,23 @@ class PortDataReceived
                        object sender,
                        SerialDataReceivedEventArgs e)
     {
-        string request = "r800000000";
-        string response = "ABCD1234";
+        int count = 0;
+        string[] requests = { "r100000000", "r200000000", "r300000000", "r400000000",
+                "r500000000", "r600000000", "r700000000", "r800000000", "r900000000"};
+        string[] responses = { "ABCD1234", "ADSS3444", "QWER4321", "WRRRRERRE",
+                    "ASDFGHJKL", "33444", "TYUI4567", "ZXCV9876", "MNBV0000" };
         SerialPort sp = (SerialPort)sender;
         string indata = sp.ReadExisting();
         Console.WriteLine(indata);
-        if (indata.Contains(request))
+        while (count < 8)
         {
-            sp.WriteLine(response);
+            if (indata.Contains(requests[count]))
+            {
+                sp.WriteLine(responses[count]);
+            }
+            count++;
         }
+        
 
     }
 }
